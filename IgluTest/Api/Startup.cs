e07c01eed8ffde1272e7c1ab884539880f0254d9
seanfdonato <seanfdonato@gmail.com>
2019-11-23 13:14:53 +0000

    Iglu
    Beggin inglu teste

---
+++ IgluTest/Api/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Configurando o serviço de documentação do Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "Iglu Test",
+                        Version = "v1",
+                        Description = "Test for Iglu Company",
+                        Contact = new Contact
+                        {
+                            Name = "Sean F. Donato"
+                        }
+                    });
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            // Habilitar o middleware para servir o Swagger gerado como um endpoint JSON
+            app.UseSwagger();
+
+            // Habilitar o middleware para servir o swagger-ui (HTML, JS, CSS, etc.), 
+            // Especificando o Endpoint JSON Swagger.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Comparador de Fundos - Meuportfol.io");
+                c.RoutePrefix = string.Empty; //Adicione algum proefixo da URL caso queira
+            });
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
---
+++ IgluTest/Domain/Entities/Address.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Address
+    {
+        public Address()
+        {
+
+        }
+        static Address()
+        {
+
+        }
+        public string PostCode { get; set; }
+        public string HouseNumber { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
